b7c20c40aac374b2f688aaf2d219d2819a3a1357
Leandro Letoile <leandroletoile@hotmail.com>
2023-01-20 19:08:54 -0300

    rutas

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,24 @@
 import GoBack from "@/components/Buttons/GoBack";
 import RegisterForm from "@/components/Forms/Register";
+import { useRouter } from "next/navigation"
+import { useContext , useEffect } from "react";
+import { UserContext } from '../../context/UserContext'
+
 
 export default function Register() {
+
+  // hacer useeffect leyendo el contexto y si el user.id esta vacio, mandar el user directamente a la de clima
+
+  const { user } = useContext(UserContext)
+
+  const router = useRouter()
+
+  useEffect(() => {
+
+    if (user.id) router.push("/weather")
+  }, [])
+
+
   return (
     <main>
       <GoBack />
